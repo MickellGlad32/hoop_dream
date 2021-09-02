3dc78216c64df6c43cf63a2ce1e64dfa10ded817
Mickell Gladness <mickellgladness@gmail.com>
2021-09-02 15:16:31 -0400

    Double Api successful

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
-// import './SearchPage.css';
+import StatsPage from './pages/StatsPage';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         </Route>
         <Route path ='/search'>
           <SearchPage/>
+        </Route>
+        <Route path = '/stats/:id'>
+          <StatsPage/>
         </Route>
       </Switch>
 
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,66 +1,78 @@
 import { useState } from 'react'
-import { Image, Col } from 'react-bootstrap'
-// import './SearchPage.css';
+import { Image, Col, Container, Row } from 'react-bootstrap'
+import './Pages.css';
+
+
 
 
 
 function SearchPage() {
     const [search, setSearch] = useState('');
     const [player, setPlayer] = useState([]);
+    const [playerID, setPlayerID] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // fetch(`https://www.balldontlie.io/api/v1/players?search=${search}`)
+
         fetch(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${search}`)
             .then(res => res.json())
             .then((data) => {
-                console.log(data)
                 setPlayer(data.player[0]);
             }, [])
+
+        fetch(`https://www.balldontlie.io/api/v1/players?search=${search}`)
+            .then(res => res.json())
+            .then(playerData => {
+                console.log('Player Detail', playerData.data)
+                setPlayerID(playerData.data[0].id)
+            })
     }
-    console.log(player)
+
+    console.log(playerID)
+
+
     return (
 
+
+
+
+
         <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <input onChange={(e) => setSearch(e.target.value)} type="text" value={search} placeholder="Search for your favorite player ie.Lebron James" />
-                    <button type="submit">Search</button>
-                </label>
-                <br />
-                {/* <label>
-                    <select>
-                        <option>Player</option>
-                        <option>Team</option>
-                    </select>
-                </label> */}
-            </form >
-            {/* <div className="full-screen-video-container">
-                <video autoplay loop muted>
-                    <source src="" type="video/mp4"/>
+            <div className="full-screen-video-container">
+                <video autoPlay loop muted>
+                    <source src="/video/Thunderstorm.mp4" type="video/mp4" />
                 </video>
-                </div> */}
                 <div className="full-screen-video-content">
-                <Col xs={6} md={4}>
-                    <Image src={player.strRender} roundedCircle />
-                </Col>
-                <h2>{player.strPlayer}</h2>
-                <h3>{player.strTeam}</h3>
-                <p>{player.dateBorn}</p>
-                <p>{player.strBirthLocation}</p>
-                <a>{player.strHeight}</a><br />
-                <a>{player.strWeight}</a>
+                    <form onSubmit={handleSubmit} className="search-form">
+                        <label>
+                            <input onChange={(e) => setSearch(e.target.value)} type="text" value={search} placeholder="Search for your favorite player ie.Lebron James" />
+                            <button type="submit">Search</button>
+                        </label>
+                    </form>
+                    <br />
+                    <Container >
+                        <Row>
+                            <Col xs={6} md={8}>
+                                <Image src={player.strRender} fluid />
+                            </Col>
+                            <Col xs={6} md={6}>
+                                <h2>{player.strPlayer}</h2>
+                                <h3>{player.strTeam}</h3>
+                                <p>{player.dateBorn}</p>
+                                <p>{player.strBirthLocation}</p>
+                                <p>{player.strHeight}</p><p>{player.strWeight}</p>
+                                <a href={`/stats/${playerID}`}>Player Stats</a>
+                            </Col>
 
+                        </Row>
+                    </Container>
+                    <form />
+                </div>
             </div>
-            {/* <div>
-            <h3>{player.first_name} {player.last_name}</h3>
-            <a>{player.height_feet}' {player.height_inches}"</a>
-            <p>{player.team.full_name}</p>
-            
-            </div> */}
-
-
         </>
+
+
+
 
 
 
@@ -69,3 +81,17 @@ function SearchPage() {
 }
 
 export default SearchPage
+
+
+{/* <form onSubmit={handleSubmit}>
+                <label>
+                    <input onChange={(e) => setSearch(e.target.value)} type="text" value={search} placeholder="Search for your favorite player ie.Lebron James" />
+                    <button type="submit">Search</button>
+                </label>
+                <br /> */}
+{/* <label>
+                    <select>
+                        <option>Player</option>
+                        <option>Team</option>
+                    </select>
+                </label> */}
